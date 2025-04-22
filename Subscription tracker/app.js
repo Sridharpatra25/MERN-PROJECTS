bd2d04cc95d91b2bded2d6d662611e8d3704b712
A Sridhar Patra <sridharpatra001@gmail.com>
2025-04-22 19:45:21 +0530

    first commit

---
+++ Subscription tracker/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+app.listen(PORT, ()=>{
+  console.log(`Server is running on port http://localhost:${PORT}`);
+})
